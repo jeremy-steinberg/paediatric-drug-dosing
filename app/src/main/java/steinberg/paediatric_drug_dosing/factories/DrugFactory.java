ef61f5e6a77851547ef87de0e2557b0a71ba7ab9
gideon-steinberg <gideon_steinberg@hotmail.com>
2018-01-10 16:15:37 +1300

    Add drug specific code

--- app/src/main/java/steinberg/paediatric_drug_dosing/factories/DrugFactory.java
+++ app/src/main/java/steinberg/paediatric_drug_dosing/factories/DrugFactory.java
@@ -9,12 +9,19 @@ public class DrugFactory {
 
         drugs.add(new Drug("Paracetamol120", "Paracetamol 120mg/5mL (15mg/kg)", "FOUR times a day", 0.625, 15, 42, 1000));
         drugs.add(new Drug("Paracetamol250", "Paracetamol 250mg/5mL (15mg/kg)","FOUR times a day", 0.3, 15, 20, 1000));
+        drugs.add(new Drug("Macrogol", "Macrogol 13.12g/sachet (0.5-1.5g/kg)","ONCE daily", 0.038, 0.5, 8, 105));
 
-        Drug drug = new Drug("Prednisolone", "Prednisolone 5mg/1mL (1-2/kg)", "ONCE daily", 0.2, 1, 8, 40);
+        Drug drug = new Drug("Prednisolone", "Prednisolone 5mg/1mL (1-2mg/kg)", "ONCE daily", 0.2, 1, 8, 40);
         drug.MessageMillileters = "\nIf child has been taking an oral corticosteroid for more than a few days, give 2 mg/kg (i.e. 0.4mL/kg, with a higher maximum of 12mL) once daily";
         drug.MessageMilligrams = "\nIf child has been taking an oral corticosteroid for more than a few days, give 2 mg/kg (higher maximum of 60mg) once daily";
         drug.HighMillilitersMaximum = 8;
         drug.HighMilligramsMaximum = 40;
+        drugs.add(drug);
+
+        drug = new Drug("Ibuprofen100", "Ibuprofen100 100mg/5mL (5-10mg/kg)", "THREE times a day", 0.25, 5, 10, 200);
+        // enable range so set a really high max
+        drug.HighMillilitersMaximum = 100000000;
+        drug.HighMilligramsMaximum = 100000000;
         drugs.add(drug);
 
         return drugs;
--- app/src/main/java/steinberg/paediatric_drug_dosing/services/CalculationService.java
+++ app/src/main/java/steinberg/paediatric_drug_dosing/services/CalculationService.java
@@ -20,12 +20,34 @@ public class CalculationService {
         }
 
         StringBuilder result = new StringBuilder();
-        result.append(totalString + " mLs " + drug.FrequencyMessage);
 
-        if (drug.MessageMillileters != null && !drug.MessageMillileters.isEmpty())
+        if (drug.Name == "Macrogol")
+        {
+            result.append(totalString + " sachets " + drug.FrequencyMessage);
+            result.append(" (disolved in " + MacrogolRounding(125 * totalUnrounded, 10) +  " mLs of water)" );
+            result.append("\nOR\nDisimpaction Dose: " + MacrogolRounding(Math.min(8, totalUnrounded * 3), 0.25));
+            result.append(" sachets daily until passes soft stool (disolved in " + MacrogolRounding(Math.min(8 * 125, totalUnrounded * 3 * 125), 10) + "mLs of water)");
+        }
+        else
+        {
+            result.append(totalString + " mLs " + drug.FrequencyMessage);
+        }
+
+
+        if (drug.Name == "Ibuprofen100" && weight < 10)
+        {
+            result.append("\nMaximum " + formatter.format(totalUnrounded) + " mLs if under THREE months");
+        }
+        else if (drug.Name == "Prednisolone")
+        {
+            double veryHighDoseUnrounded = Math.min(12, weight * 0.4);
+            result.append("\nIf child has already been taking for more than a few days, give " + formatter.format(veryHighDoseUnrounded) + "mL once daily (2mg/kg with a 60mg/12mL maximum)");
+        }
+        else if (drug.MessageMillileters != null && !drug.MessageMillileters.isEmpty())
         {
             result.append(" " + drug.MessageMillileters);
         }
+
 
         return result.toString();
     }
@@ -44,11 +66,31 @@ public class CalculationService {
         StringBuilder result = new StringBuilder();
         result.append(totalString + " mgs " + drug.FrequencyMessage);
 
-        if (drug.MessageMilligrams != null && !drug.MessageMilligrams.isEmpty())
+        if (drug.Name == "Ibuprofen100" && weight < 10)
+        {
+            result.append("\nMaximum " + formatter.format(totalUnrounded) + " mLs if under THREE months");
+        }
+        else if (drug.Name == "Prednisolone")
+        {
+            double veryHighDoseUnrounded = Math.min(60, weight * 2);
+            result.append("\nIf child has already been taking for more than a few days, give " + formatter.format(veryHighDoseUnrounded) + "mg once daily (2mg/kg with a 60mg maximum)");
+        }
+        else if (drug.Name == "Macrogol")
+        {
+            result.append("\n (or " + MacrogolRounding(Math.min(105, (totalUnrounded * 3)), 0.25) + " mgs daily for disimpaction until passes soft stool)");
+        }
+        else if (drug.MessageMilligrams != null && !drug.MessageMilligrams.isEmpty())
         {
             result.append(" " + drug.MessageMilligrams);
         }
 
         return result.toString();
     }
+
+    private static String MacrogolRounding(double value, double step)
+    {
+        double inv = 1.0/ step;
+        return formatter.format(Math.round(value * inv) / inv);
+    }
+
 }
